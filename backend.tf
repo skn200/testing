ee434448ef4dbb21486a419417bd1ff9a8f423d9
skn200 <58088308+skn200@users.noreply.github.com>
2019-11-23 20:20:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "TF-BKP-RG"
     storage_account_name = "sagtftest2"
     container_name       = "tfstate2"
-    key                  = "my.terraform2.tfstate"
+    key                  = "my.terraform22.tfstate"
   }
 }
